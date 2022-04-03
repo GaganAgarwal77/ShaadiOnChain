eaf9888a2f39a0d096fddf531444f7a8c7e771b0
GaganAgarwal77 <Gagan.Agarwal@iiitb.ac.in>
2022-04-03 20:18:18 +0530

    Replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 , document.getElementById('root'));
 
 serviceWorker.unregister();
